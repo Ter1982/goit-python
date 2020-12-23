738faf6feab8044715006cfed48135fe7637a79a
misha <mter1982@gmail.com>
2020-12-23 02:07:26 +0200

    Hw_3

--- Lesson2/Hw_3.py
+++ Lesson2/Hw_3.py
@@ -1,28 +1,42 @@
-
-
 while True:
-    
-        x = float(input("Enter number:"))
-   
-        user_input = input("Enter operator (+,-,*,/): ")
 
-        y = float(input("Enter number:"))
+    try:
+        x = float(input('Введите первое число:'))
+    except ValueError:
+        print('неправильный ввод числа')
+        continue
+    while True:
 
-        z = input("Enter operator( = ):")
-        if user_input == '+':
-            print(x + y)
-        elif user_input == '-':
-            print(x - y)
-        elif user_input == '*':
-            print(x * y)
-        elif user_input == '/':
-            if y != 0:
-                print(x / y)
-            else:
-                print("Деление на ноль!")
-        else:
-            print("Не верный ввод") 
-                   
-               
-        
-    
+        user_input = input('Введите оператор (+,-,*,/): ')
+        if user_input not in ('+', '-', '*', '/'):
+            print('неверный оператор выражения')
+            continue
+        while True:
+
+            try:
+                y = float(input('Введите второе число:'))
+            except ValueError:
+                print('неправильный ввод числа')
+                continue
+            while True:
+                z = input('Введите оператор( = ):')
+                if z != '=':
+                    print('неверный оператор ввода ровно')
+                    continue
+
+                if user_input == '+':
+                    print(x + y)
+                elif user_input == '-':
+                    print(x - y)
+                elif user_input == '*':
+                    print(x * y)
+                elif user_input == '/':
+                    if y != 0:
+                        print(x / y)
+                    else:
+                        print('Деление на ноль')
+
+                break
+            break
+        break
+    break
